032f10c907e2661ad45a46bc38668b34648a6c81
Sarita15 <saritagarcias15@gmail.com>
2020-05-23 14:57:30 -0500

    Primer Commit

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+         host: 'sql9.freemysqlhosting.net',
+        user: ' sql9336441',
+        password: '9tW7Au7hPn',
+        database: ' sql9336441'
+    }
+
+};
